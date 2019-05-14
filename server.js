f2c3e30f8f6fffa5ebe90c9b750dd5b2bddef83c
Sean Massingill <25423602+historybro@users.noreply.github.com>
2019-05-13 20:51:04 -0500

    updated server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var PORT = process.env.PORT || 8080;
 var app = express();
+var exphbs = require("express-handlebars");
 
 app.use(express.static("public"));
 app.use(express.static(__dirname+'/public'));
@@ -8,15 +9,13 @@ app.use(express.static(__dirname+'/public'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-var exphbs = require("express-handlebars");
-
 app.engine("handlebars", exphbs({defaultLayout: "main"}));
 app.set("view engine", "handlebars");
 
-var routes = require("./controllers/burger_controller");
+require("./controllers/burger_controller")(app);
 
-app.use(routes);
-
-app.listen(PORT, function(){
-    console.log("Server listening on http://localhost:"+PORT);
-})
+db.sequelize.sync({ force: true }).then(function() {
+    app.listen(PORT, function(){
+        console.log("Server listening on http://localhost:"+PORT);
+    });
+});  
